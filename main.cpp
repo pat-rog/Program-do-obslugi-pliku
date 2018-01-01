ed25bf32684b6f8abab4372a64dfc75056f625f2
=pat-rog <pat.rogaczewski@gmail.com>
2018-01-02 00:56:37 +0100

    Dodałem funkcję comparison_change, która ustawia pliki zgodnie z kolejnością od ostatniej modyfikacji. W kodzie jest jeden błąd - algorytm nie radzi sobie w sytuacji gdy jest ta sama data i godzina. Do poprawy!

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ void show_dic(char name[100]);
 void show_file(char name[100]);
 void comparison_size(char name[100], int licznik);
 void licznik_plikow(char name[100], int &licznik);
+void comparison_change(char name[100], int licznik);
 
 
 
@@ -104,6 +105,7 @@ void menu(char name[100], int licznik)
         }
         case 4:
         {
+            comparison_change(name,licznik);
             break;
         }
         case 5:
@@ -278,7 +280,7 @@ void licznik_plikow(char name[100], int &licznik)
         start();
     }
 }
-//Porównaj wielkość wybranych plików
+//Porównaj wielkość wybranych plikówc
 void comparison_size(char name[100], int licznik)
 {
     licznik_plikow(name,licznik);
@@ -378,6 +380,120 @@ void comparison_size(char name[100], int licznik)
         delete[] tab2;
         delete[] value;
         delete[] value_choosen;
+    }
+    else
+    {
+        start();
+    }
+}
+//Porównaj datę i czas modyfikacji wybranych plików
+void comparison_change(char name[100], int licznik)
+{
+    licznik_plikow(name,licznik);
+    int licznik_2 = 0;
+    fstream file;
+    string line_1, line_2, line_3, line_4;
+    string *tab1 = new string[licznik];  //nazwy plików
+    int *tab3 = new int[licznik];
+    file.open(name, ios::in | ios::out);
+    if(file.good() == true)
+    {
+        while(!file.eof())
+        {
+            getline(file, line_1);
+            if(line_1.size()!=1 && line_1.size()!=0)
+            {
+                if(line_1.erase(13) == " Directory of") //|| line == "Directory of")
+                {
+                    while(!file.eof())
+                    {   
+                        getline(file, line_2);
+                        if(line_2.size()!=1 && line_2.size()!=0)
+                        {
+                            line_4 = line_2;
+                            line_3 = line_2;
+                            line_3.erase(0, 21);
+                            line_3.erase(5);
+                            if(line_3.erase(5) == "     ")
+                            {
+                                //Pobieranie nazwy pliku
+                                line_2.erase(0,36);
+                                tab1[licznik_2] = line_2;
+                                //Pobieranie daty i godziny
+                                line_4.erase(2,1);
+                                line_4.erase(4,1);
+                                line_4.erase(8,2);
+                                line_4.erase(10,1);
+                                line_4.erase(12);
+                                tab3[licznik_2] = atoi(line_4.c_str());
+                                licznik_2 = licznik_2 + 1;
+                                line_2 = ""; //uwzględnia fakt, ze zawartosc pliku większa od przeznaczonego miejsca na program
+                            }
+                        }
+                    }
+                } 
+            }
+            line_1= ""; //uwzględnia fakt, ze zawartosc pliku większa od przeznaczonego miejsca na program
+        }
+        file.close();
+        cout << "Lista plików: " << endl;
+        //Przydziela numery plikom
+        for(int j=0; j<licznik; j++)
+        {
+          cout << j << "." << tab1[j] << endl;
+        }
+        cout << "Ilość plików, które chcesz porównać: ";
+        int ile;
+        cin >> ile;
+        int *tab2 = new int[ile];
+        //Pobiera od użytkownika numery plików
+        for(int k=0; k<ile; k++)
+        {
+            cout << "Numer wybranego pliku: ";
+            cin >> tab2[k];
+        }
+        int l; //zmienna pomocnicza
+        int rozmiar;
+        rozmiar = ile - 1; // zmienna do algorytmu bąbelkowego
+        int *date_choosen = new int[ile];
+        for(int m=0; m<ile; m++)
+        {
+            l = tab2[m];
+            date_choosen[m] = tab3[l];
+        }
+        //algorytm sortujący wielkości wybranych plików od najmniejszej
+        int bufor;
+        for(int a=0; a<rozmiar; a++)
+        {
+            for(int b=0; b<rozmiar; b++)
+            {
+                if(date_choosen[b]<date_choosen[b+1])
+                {
+                    bufor = date_choosen[b+1];
+                    date_choosen[b+1] = date_choosen[b];
+                    date_choosen[b] = bufor;
+                }
+                //else if(date_choosen[b]==date_choosen[b+1])
+                //{
+                    //brak zmian
+                //}
+            }
+        }
+        cout << "Wybrane pliki w kolejności od ostaniej modyfikacji" << endl;
+        for(int d=0; d<ile; d++)
+        {
+            for(int e=0; e<licznik; e++)
+            {
+                if(date_choosen[d]==tab3[e])
+                {
+                   cout << tab1[e] << endl;
+                }
+            }
+        }
+        delete[] tab1;
+        delete[] tab2;
+        delete[] tab3;
+        delete[] date_choosen;
     }
     else
     {
